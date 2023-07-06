3dc14999b88a9de18be8c988af9dcc639ddaca8b
GalaxyCLDev <118214204+GalaxyCLDev@users.noreply.github.com>
2023-07-06 00:10:57 -0400

    Update login.php
    modificación para evitar inyección sql

--- nueva/front/back_end/login.php
+++ nueva/front/back_end/login.php
@@ -12,25 +12,49 @@ if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
 }
 
-// Recibir los datos del formulario
-$correo = $_POST["correo"];
-$contrasena = $_POST["contrasena"];
-
-// Verificar las credenciales del usuario
-$sql = "SELECT ID, Nombre FROM Usuario WHERE Correo = '$correo' AND Contraseña = '$contrasena'";
-
-$result = $conn->query($sql);
-
-if ($result->num_rows == 1) {
-    // Inicio de sesión exitoso
-    $row = $result->fetch_assoc();
+// Verificar si se solicita cerrar sesión
+if (isset($_GET['logout'])) {
+    // Destruir todas las variables de sesión
     session_start();
-    $_SESSION["usuario_id"] = $row["ID"];
-    $_SESSION["usuario_nombre"] = $row["Nombre"];
-    header("Location: menu.html"); // Redireccionar al menú
-} else {
-    // Credenciales incorrectas
-    echo "Inicio de sesión fallido. Verifica tus credenciales.";
+    session_unset();
+    session_destroy();
+
+    // Redirigir al inicio de sesión
+    header("Location: inicio_sesion.html");
+    exit();
+}
+
+// Recibir los datos del formulario
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $correo = $_POST["correo"];
+    $contrasena = $_POST["contrasena"];
+
+    // Validar y limpiar los datos de entrada
+    $correo = mysqli_real_escape_string($conn, $correo);
+
+    // Verificar las credenciales del usuario utilizando sentencias preparadas
+    $sql = "SELECT ID, Nombre FROM Usuario WHERE Correo = ? AND Contraseña = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("ss", $correo, $contrasena);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if ($result->num_rows == 1) {
+        // Inicio de sesión exitoso
+        $row = $result->fetch_assoc();
+        session_start();
+        $_SESSION["usuario_id"] = $row["ID"];
+        $_SESSION["usuario_nombre"] = $row["Nombre"];
+        header("Location: menu.html"); // Redireccionar al menú
+        exit();
+    } else {
+        // Credenciales incorrectas
+        $error_message = "Inicio de sesión fallido. Verifica tus credenciales.";
+        // Redirigir a una página de error o mostrar el mensaje de error en una plantilla de vista
+        // ...
+    }
+
+    $stmt->close();
 }
 
 $conn->close();
